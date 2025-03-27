7e93d7042a61e581da99ed66c2de36778c5c0bed
julianort11 <ort.na140@gmail.com>
2025-03-27 18:23:46 -0500

    feat: :sparkles: add pets

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
+ import {addPets} from "./registro.js";
 
 
+ let opcion;
+ do {
+     opcion = prompt(
+     "Bienvenidos a la veterianaria:)\n"+
+     "Menú:\n" +
+     "a. Registrar una nueva mascota.\n" +
+     "b. Listar todas las mascotas registradas.\n" +
+     "c. Buscar una mascota por nombre.\n" +
+     "d. Actualizar el estado de salud de una mascota.\n" +
+     "e. Eliminar una mascota por nombre.\n" +
+     "f. Salir\n" +
+     "Elige una opción:"
+   );
 
-let opcion;
-do {
-    opcion = prompt(
-    "Bienvenidos a la veterianaria:)\n"+
-    "Menú:\n" +
-    "a. Registrar una nueva mascota.\n" +
-    "b. Sumar dos números.\n" +
-    "c. Buscar una mascota por nombre.\n" +
-    "d. Actualizar el estado de salud de una mascota.\n" +
-    "e. Eliminar una mascota por nombre.\n" +
-    "f. Salir\n" +
-    "Elige una opción:"
-  );
-
-    switch (opcion) {
-        case "a":
-          alert("a. Registrar una nueva mascota.");
-          break;
-        case "b":
-          alert("b. Sumar dos números.")
-          break;
-        case "c":
-          alert("c. Buscar una mascota por nombre.")
-          break;
-        case "d":
-          alert("d. Actualizar el estado de salud de una mascota.")
-          break;
-        case "e":
-          alert("e. Eliminar una mascota por nombre.")
-          break;
-        case "f":
-          alert("f. Salir");
-          break;
-        default:
-          alert("Opción no válida, intenta de nuevo.");
-  }
-} while (opcion !== "f");
+     switch (opcion) {
+         case "a":
+           addPets()
+           break;
+         case "b":
+           alert("b. Listar todas las mascotas registradas.")
+           break;
+         case "c":
+           alert("c. Buscar una mascota por nombre.")
+           break;
+         case "d":
+           alert("d. Actualizar el estado de salud de una mascota.")
+           break;
+         case "e":
+           alert("e. Eliminar una mascota por nombre.")
+           break;
+         case "f":
+           alert("f. Salir");
+           break;
+         default:
+           alert("Opción no válida, intenta de nuevo.");
+   }
+ } while (opcion !== "f");
 
 console.log("Programa terminado.");
--- js/registro.js
+++ js/registro.js
@@ -1,3 +1,23 @@
-const aggMAscotas = () => {
-    
+let pets= []
+
+export function addPets() {
+    let Nombre = prompt("nombre de la mascota: ");
+    let Especie = prompt("Ingrese la especie de la mascota: ");
+    let Edad = prompt("Ingrese la edad de la mascota: ");
+    let Peso = prompt("Ingrese el peso de la mascota: ");
+    let Estado = prompt("Ingrese el estado de su mascota:\n"+"Sano\n"+"Enfermo\n"+"En tratamiento");
+
+    const newPet ={
+        Nombre : Nombre,
+        Especie : Especie,
+        Edad : Edad,
+        Peso : Peso,
+        Estado : {Sano: true,
+            Enfermo: false,
+            Tratamiento: false
+        }
+    };
+
+    pets.push(newPet);
+    alert("La mascota resgistrada con exito")
 }
